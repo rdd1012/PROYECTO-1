c267fed744ed0fbbcc8042c2b87589918c54428a
rdd1012 <rdd1012@alu.ubu.es>
2025-04-26 13:42:52 +0200

    giro sin fallos

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,37 +38,56 @@ public class PlayerController : MonoBehaviour {
 
         StartCoroutine(itemvisuals.SelectAnimation());
 
+        Vector2 targetPoint = new Vector2(item.GoToPoint.position.x, transform.position.y);
+
+        if (Vector2.Distance(transform.position, targetPoint) <= 0.01f)
+        {
+            Interact(interactableItem);
+            return;
+        }
+
         if (!isMoving)
         {
             StartCoroutine(MoveToPoint(
-                item.GoToPoint.position, 
-                item.GetComponentInChildren<InteractableVisuals>().gameObject.transform.position, 
+                item.GoToPoint.position,
+                item.GetComponentInChildren<InteractableVisuals>().gameObject.transform.position,
                 interactableItem
-                ));
+            ));
         }
     }
-    public IEnumerator MoveToPoint(Vector2 point, Vector2 pointVisuals , IInteractable interactable)
+    public IEnumerator MoveToPoint(Vector2 point, Vector2 pointVisuals, IInteractable interactable)
     {
         Vector2 targetPoint = new Vector2(point.x, transform.position.y);
 
         isMoving = true;
         anim.Play("Caminar");
         anim.SetBool("EstaCaminando", true);
+        Vector2 targetPointVisuals = new Vector2(pointVisuals.x, transform.position.y);
+        float direction = Mathf.Sign(targetPointVisuals.x - transform.position.x);
+
+        if (direction != Mathf.Sign(transform.localScale.x))
+        {
+            Vector3 newScale = transform.localScale;
+            newScale.x = Mathf.Abs(newScale.x) * direction;
+            transform.localScale = newScale;
+        }
 
         while (Vector2.Distance(transform.position, targetPoint) > 0f)
         {
-            Vector2 targetPointVisuals = new Vector2(pointVisuals.x, transform.position.y);
-            float direction = Mathf.Sign(targetPointVisuals.x - transform.position.x);
-
-            if (direction != Mathf.Sign(transform.localScale.x))
-            {
-                Vector3 newScale = transform.localScale;
-                newScale.x = Mathf.Abs(newScale.x) * direction;
-                transform.localScale = newScale;
-            }
-
             transform.position = Vector2.MoveTowards(transform.position, targetPoint, speed * Time.deltaTime);
             yield return null;
+        }
+
+        // Solo gira si hay diferencia en la posición X
+        if (!Mathf.Approximately(targetPointVisuals.x, targetPoint.x))
+        {
+            float lookDirection = Mathf.Sign(targetPointVisuals.x - transform.position.x);
+            if (lookDirection != Mathf.Sign(transform.localScale.x))
+            {
+                Vector3 newScale = transform.localScale;
+                newScale.x = Mathf.Abs(newScale.x) * lookDirection;
+                transform.localScale = newScale;
+            }
         }
 
         isMoving = false;
