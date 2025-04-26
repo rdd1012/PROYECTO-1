2b0a7ef9e324f41256bedae2e00289dd454c2438
rdd1012 <152408956+rdd1012@users.noreply.github.com>
2025-04-26 13:45:52 +0200

    Revert "Girar sprite"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     public static PlayerController Instance;
-    bool isMoving = false;
+    bool isMoving=false;
     Animator anim;
     private void Start()
     {
@@ -22,77 +22,46 @@ public class PlayerController : MonoBehaviour {
             Destroy(gameObject);
         }
     }
-    private float speed = 10f;
-    private bool isMovingLeft = false;
+        private float speed = 10f;
+    private bool isMovingLeft=false;
     public bool IsMovingLeft { get { return isMovingLeft; } }
 
     public void GoToItem(InteractableData item)
     {
-        if (item == null || item.GoToPoint == null) return;
+        if (item == null || item.GoToPoint == null) return; 
 
         InteractableVisuals itemvisuals = item.GetComponentInChildren<InteractableVisuals>();
-        if (itemvisuals == null) return;
+        if (itemvisuals == null) return; 
 
         IInteractable interactableItem = item.GetComponent<IInteractable>();
         if (interactableItem == null) return;
 
         StartCoroutine(itemvisuals.SelectAnimation());
 
-        Vector2 targetPoint = new Vector2(item.GoToPoint.position.x, transform.position.y);
-
-        if (Vector2.Distance(transform.position, targetPoint) <= 0.01f)
-        {
-            Interact(interactableItem);
-            return;
-        }
-
         if (!isMoving)
         {
-            StartCoroutine(MoveToPoint(
-                item.GoToPoint.position,
-                item.GetComponentInChildren<InteractableVisuals>().gameObject.transform.position,
-                interactableItem
-            ));
+            StartCoroutine(MoveToPoint(item.GoToPoint.position, interactableItem));
         }
     }
-    public IEnumerator MoveToPoint(Vector2 point, Vector2 pointVisuals, IInteractable interactable)
+    public IEnumerator MoveToPoint(Vector2 point, IInteractable interactable)
     {
         Vector2 targetPoint = new Vector2(point.x, transform.position.y);
-
-        isMoving = true;
-        anim.Play("Caminar");
-        anim.SetBool("EstaCaminando", true);
-        Vector2 targetPointVisuals = new Vector2(pointVisuals.x, transform.position.y);
-        float direction = Mathf.Sign(targetPointVisuals.x - transform.position.x);
-
-        if (direction != Mathf.Sign(transform.localScale.x))
-        {
-            Vector3 newScale = transform.localScale;
-            newScale.x = Mathf.Abs(newScale.x) * direction;
-            transform.localScale = newScale;
-        }
-
+        
+        //if (targetPoint.x != transform.position.x) 
+        isMovingLeft = targetPoint.x < transform.position.x;
         while (Vector2.Distance(transform.position, targetPoint) > 0f)
         {
+            isMoving = true;
+            anim.Play("Caminar");
+            anim.SetBool("EstaCaminando", true);
+
             transform.position = Vector2.MoveTowards(transform.position, targetPoint, speed * Time.deltaTime);
             yield return null;
         }
-
-        // Solo gira si hay diferencia en la posición X
-        if (!Mathf.Approximately(targetPointVisuals.x, targetPoint.x))
-        {
-            float lookDirection = Mathf.Sign(targetPointVisuals.x - transform.position.x);
-            if (lookDirection != Mathf.Sign(transform.localScale.x))
-            {
-                Vector3 newScale = transform.localScale;
-                newScale.x = Mathf.Abs(newScale.x) * lookDirection;
-                transform.localScale = newScale;
-            }
-        }
-
         isMoving = false;
         anim.Play("Idle");
         anim.SetBool("EstaCaminando", false);
+
         transform.position = targetPoint;
 
         if (interactable != null)
