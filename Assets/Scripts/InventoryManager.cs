39ea0be0fcfc48c9a5b3f1245c3c9f84111be2b1
rdd1012 <rdd1012@alu.ubu.es>
2025-04-26 22:59:13 +0200

    nombresinventario
    cuando pasas el raton por encima se muestra el nombre de los objetos dle inventario VAMOOOO

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,40 +1,135 @@
+using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventoryManager : MonoBehaviour
-{
+public class InventoryManager : MonoBehaviour {
     [SerializeField] private Color selectedSlotColor = Color.grey;
-    public static InventoryManager Instance;
     [SerializeField] private List<Item> items = new List<Item>();
     public List<Item> Items { get { return items; } set { items = Items; } }
-    [SerializeField]private GameObject[] itemSlots;
+    [SerializeField] private GameObject[] itemSlots;
     [SerializeField] private Image[] slotImages;
     [SerializeField] private Item selectedItem;
     public Item SelectedItem { get { return selectedItem; } set { selectedItem = SelectedItem; } }
+
+    [SerializeField] private GameObject tooltipPanel;
+    [SerializeField] private TMP_Text tooltipText;
+    [SerializeField] private Vector2 tooltipOffset = new Vector2(30, -30);
+    [SerializeField] private float tooltipDelay = 0.1f;
+
+    public static InventoryManager Instance;
     private int selectedSlotIndex = -1;
+    private bool isMouseOverSlot = false;
+    private Coroutine showTooltipCoroutine;
 
     void Awake()
     {
         Instance = this;
         InitializeSlots();
+        InitializeTooltip();
     }
+
+    void Update()
+    {
+        if (tooltipPanel.activeSelf)
+        {
+            UpdateTooltipPosition();
+        }
+    }
+
+    void InitializeTooltip()
+    {
+        if (tooltipPanel != null)
+        {
+            tooltipPanel.SetActive(false);
+            
+            var tooltipImage = tooltipPanel.GetComponent<Image>();
+            if (tooltipImage != null) tooltipImage.raycastTarget = false;
+            tooltipText.raycastTarget = false;
+        }
+    }
+
+    void UpdateTooltipPosition()
+    {
+        RectTransform tooltipRect = tooltipPanel.GetComponent<RectTransform>();
+        Canvas canvas = GetComponent<Canvas>();
+
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.GetComponent<RectTransform>(),
+            Input.mousePosition,
+            canvas.worldCamera,
+            out localPoint
+        );
+
+        tooltipRect.localPosition = localPoint + tooltipOffset;
+    }
+
+    public void ShowNombreInventario(int slotIndex)
+    {
+        if (slotIndex < 0 || slotIndex >= items.Count || items[slotIndex] == null) return;
+
+        tooltipPanel.transform.SetAsLastSibling();
+        tooltipText.text = items[slotIndex].itemName;
+        tooltipPanel.SetActive(true);
+        UpdateTooltipPosition();
+    }
+
+    private void HideNombreInventario()
+    {
+        tooltipPanel.SetActive(false);
+    }
+
     private void InitializeSlots()
     {
         for (int i = 0; i < itemSlots.Length; i++)
         {
             int index = i;
+
+            
             itemSlots[i].GetComponent<Button>().onClick.AddListener(() => SelectItem(index));
+
+            
+            EventTrigger trigger = itemSlots[i].GetComponent<EventTrigger>() ?? itemSlots[i].AddComponent<EventTrigger>();
+            trigger.triggers.Clear();
+
+            
+            EventTrigger.Entry entryEnter = new EventTrigger.Entry();
+            entryEnter.eventID = EventTriggerType.PointerEnter;
+            entryEnter.callback.AddListener((data) => {
+                isMouseOverSlot = true;
+                showTooltipCoroutine = StartCoroutine(ShowNombreInventarioDelayed(index));
+            });
+            trigger.triggers.Add(entryEnter);
+
+            // Evento PointerExit
+            EventTrigger.Entry entryExit = new EventTrigger.Entry();
+            entryExit.eventID = EventTriggerType.PointerExit;
+            entryExit.callback.AddListener((data) => {
+                isMouseOverSlot = false;
+                if (showTooltipCoroutine != null) StopCoroutine(showTooltipCoroutine);
+                HideNombreInventario();
+            });
+            trigger.triggers.Add(entryExit);
         }
     }
+
+    private IEnumerator ShowNombreInventarioDelayed(int slotIndex)
+    {
+        yield return new WaitForSeconds(tooltipDelay);
+        if (isMouseOverSlot) ShowNombreInventario(slotIndex);
+    }
+
+    
     public void SelectItem(int slotIndex)
     {
-        
         selectedSlotIndex = slotIndex;
         selectedItem = items.Count > slotIndex ? items[slotIndex] : null;
-        if (selectedItem!=null)UpdateSlotHighlights();
+        if (selectedItem != null) UpdateSlotHighlights();
     }
+
     private void UpdateSlotHighlights()
     {
         for (int i = 0; i < slotImages.Length; i++)
@@ -42,13 +137,13 @@ public class InventoryManager : MonoBehaviour
             slotImages[i].color = (i == selectedSlotIndex) ? selectedSlotColor : Color.white;
         }
     }
+
     private void DeselectItem()
     {
-        selectedSlotIndex --;
+        selectedSlotIndex--;
         selectedItem = null;
         UpdateSlotHighlights();
     }
-
 
     private void Start()
     {
@@ -79,26 +174,12 @@ public class InventoryManager : MonoBehaviour
     {
         for (int i = 0; i < itemSlots.Length; i++)
         {
-            var slotImage = itemSlots[i].GetComponent<UnityEngine.UI.Image>();
+            var slotImage = itemSlots[i].GetComponent<Image>();
+            if (slotImage == null) continue;
 
-            if (slotImage != null)
-            {
-                if (i < items.Count && items[i].icon != null)
-                {
-                    slotImage.sprite = items[i].icon;
-                    slotImage.enabled = true;
-                }
-                else
-                {
-                    // sprite para slot vacío
-                    slotImage.sprite = null;
-                    slotImage.enabled = false; 
-                }
-            }
-            else
-            {
-                Debug.LogWarning($"Slot {i} no tiene componente Image");
-            }
+            bool hasItem = i < items.Count && items[i].icon != null;
+            slotImage.sprite = hasItem ? items[i].icon : null;
+            slotImage.enabled = hasItem;
         }
     }
 }
