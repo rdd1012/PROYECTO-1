c3314115cc4849f81c26f0e44b6c5f31d5454b77
rdd1012 <rdd1012@alu.ubu.es>
2025-04-27 11:29:16 +0200

    sistema de dialogos añadido

---
+++ Assets/Scripts/INPC.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface INPC {
+    IEnumerator Yap(string _text);
+}
---
+++ Assets/Scripts/NPCdialogoSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogo", menuName = "Dialogo/NPC Dialogo")]
+public class NPCdialogoSO : ScriptableObject {
+    public string npcName;
+    public string[]frases;
+}
