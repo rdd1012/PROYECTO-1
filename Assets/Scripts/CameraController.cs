dc164cb649dc1dc0b91a323a7051ce6816ee70ac
rdd1012 <rdd1012@alu.ubu.es>
2025-05-02 12:53:40 +0200

    flechas que indican el movimiento de la camara añadidas, falta el sprite

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,23 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-
-
+public class CameraController : MonoBehaviour {
+    private float moveSpeed = 15f;
+    private int edgeScrollSize = 50;
+    private float minX = -19f;
+    private float maxX = 19f;
+    [SerializeField] GameObject flechaDerecha;
+    [SerializeField] GameObject flechaIzquierda;
+    private void Start()
+    {
+        flechaIzquierda.SetActive(false);
+        flechaDerecha.SetActive(false);
+    }
     private void Update()
     {
-        Vector3 inputDir = new Vector3(0, 0, 0);
-         int edgeScrollSize = 20;
+        Vector3 inputDir = Vector3.zero;
 
-        if (Input.mousePosition.x < edgeScrollSize) { inputDir.x = -1f; }
-        if (Input.mousePosition.x > Screen.width - edgeScrollSize) { inputDir.x = +1f; }
-        Vector3 moveDir = transform.right * inputDir.x;
-        float moveSpeed = 15f;
 
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        if (Input.mousePosition.x < edgeScrollSize)
+        {
+            inputDir.x = -1f;
+            flechaIzquierda.SetActive(true);
+        }
+        else if (Input.mousePosition.x > Screen.width - edgeScrollSize)
+        {
+            inputDir.x = 1f;
+            flechaDerecha.SetActive(true);
+        }
+        else
+        {
+            flechaIzquierda.SetActive(false);
+            flechaDerecha.SetActive(false);
+        }
+
+
+            Vector3 moveDir = transform.right * inputDir.x;
+
+        
+        Vector3 newPosition = transform.position + moveDir * moveSpeed * Time.deltaTime;
+
+        
+        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+
+        
+        transform.position = newPosition;
     }
-
 }
-
