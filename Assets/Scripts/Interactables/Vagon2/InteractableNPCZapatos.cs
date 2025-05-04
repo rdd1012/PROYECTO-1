9671f3686d442bb2af78665a7c4b9f39e7c2a03f
rdd1012 <rdd1012@alu.ubu.es>
2025-05-04 11:41:10 +0200

    añadido

--- Assets/Scripts/Interactables/Vagon2/InteractableNPCZapatos.cs
+++ Assets/Scripts/Interactables/Vagon2/InteractableNPCZapatos.cs
@@ -1,9 +1,11 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableNPCLibro : NPCBase, IInteractable {
-    private InteractableData interactableData; 
-    [SerializeField] private Item itemToGive; 
+public class InteractableNPCZapatos : NPCBase,IInteractable
+{
+    private InteractableData interactableData;
+    [SerializeField] private Item itemToGive;
     bool teniaObjeto = false;
     private bool inventoryHasItem = false;
     AudioSource audioSource;
@@ -11,10 +13,10 @@ public class InteractableNPCLibro : NPCBase, IInteractable {
     Sprite normal;
     [SerializeField] Sprite pestañeo;
     [SerializeField] Sprite hablando;
-    [SerializeField] Sprite libro;
-    [SerializeField] private NPCdialogoSO dialogos; 
+    [SerializeField] Sprite zapatosLimpios;
+    [SerializeField] private NPCdialogoSO dialogos;
     YapBubble yapBubble;
-    private void Start()
+    void Start()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         normal = spriteRenderer.sprite;
@@ -22,14 +24,15 @@ public class InteractableNPCLibro : NPCBase, IInteractable {
         yapBubble.gameObject.SetActive(false);
         interactableData = GetComponent<InteractableData>();
         audioSource = GetComponent<AudioSource>();
-        StartCoroutine(Blink(pestañeo,spriteRenderer));
+        StartCoroutine(Blink(pestañeo, spriteRenderer));
     }
+
     public void OnClickAction()
     {
         if (interactableData.CheckItemRequirement())
         {
             if (!teniaObjeto)
-            QuitarItem(interactableData.requiredItemID);
+                QuitarItem(interactableData.requiredItemID);
 
 
             GiveItem();
@@ -37,17 +40,16 @@ public class InteractableNPCLibro : NPCBase, IInteractable {
         if (teniaObjeto) StartCoroutine(Yap(dialogos.frases[1], hablando, normal, spriteRenderer, yapBubble));
         else StartCoroutine(Yap(dialogos.frases[0], hablando, normal, spriteRenderer, yapBubble));
     }
-    public override IEnumerator Yap(string _text,Sprite _hablando,Sprite _normal,SpriteRenderer _spriteRenderer,YapBubble _yapbubble)
+    public override IEnumerator Yap(string _text, Sprite _hablando, Sprite _normal, SpriteRenderer _spriteRenderer, YapBubble _yapbubble)
     {
         _spriteRenderer.sprite = _hablando;
         _yapbubble.gameObject.SetActive(true);
         _yapbubble.SetupText(_text);
         yield return new WaitForSeconds(3f);
         _yapbubble.gameObject.SetActive(false);
-        if (teniaObjeto) _spriteRenderer.sprite = libro;
+        if (teniaObjeto) _spriteRenderer.sprite = zapatosLimpios;
         else _spriteRenderer.sprite = _normal;
     }
-
     private void QuitarItem(int itemID)
     {
 
@@ -58,10 +60,12 @@ public class InteractableNPCLibro : NPCBase, IInteractable {
                 if (_item.itemID == itemID)
                 {
                     teniaObjeto = true;
-                    InventoryManager.Instance.RemoveItem(itemID);
-                    spriteRenderer.sprite = libro;
-                    break;
+                    _item.usos--;
+                    if (_item.usos == 0) { InventoryManager.Instance.RemoveItem(itemID); }
                     
+                    spriteRenderer.sprite = zapatosLimpios;
+                    break;
+
                 }
             }
         }
@@ -83,7 +87,7 @@ public class InteractableNPCLibro : NPCBase, IInteractable {
                 InventoryManager.Instance.AddItem(itemToGive);
                 inventoryHasItem = true;
                 audioSource.Play();
-                
+
             }
 
         }
