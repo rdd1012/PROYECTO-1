6764c0604e910a4e7b15a089872b0a80b76bbfc6
rdd1012 <rdd1012@alu.ubu.es>
2025-05-08 22:56:50 +0200

    añadido

---
+++ Assets/Scripts/Interactables/Vagon3/CuadroPantalla.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CuadroPantalla : MonoBehaviour
+{
+    [SerializeField] InteractableCuadro cuadro;
+    [SerializeField] Button palanca;
+    [SerializeField] Image fondoCuadroImagen;
+    [SerializeField] Sprite spriteFeliz;
+    [SerializeField] Button botonTrigo;
+    //AudioSource audioSource;
+
+    private void Start()
+    {
+      //  audioSource = GetComponent<AudioSource>();
+        palanca.enabled = false;
+    }
+    public void LlenarTrigo()
+    {
+        if (!cuadro.TeniaObjeto)
+        {
+            if (cuadro.InteractableData.CheckItemRequirement(cuadro.InteractableData.requiredItemID))
+            {
+                cuadro.TeniaObjeto = true;
+                cuadro.QuitarItem(cuadro.InteractableData.requiredItemID);
+                fondoCuadroImagen.sprite = spriteFeliz;
+                palanca.enabled = true ;
+                Destroy(botonTrigo);
+                
+            }
+        }
+
+
+    }
+}
