1ecde6858d7e27478baf844df0b96aee1d073fca
rdd1012 <rdd1012@alu.ubu.es>
2025-05-19 10:18:06 +0200

    esta

--- Assets/Scripts/Interactables/Vagon4/PuertaPantalla.cs
+++ Assets/Scripts/Interactables/Vagon4/PuertaPantalla.cs
@@ -7,7 +7,9 @@ public class PuertaPantalla : MonoBehaviour
    
 {
     [SerializeField]InteractablePuertaHorquilla puerta;
-    [SerializeField] Image cerradura;
+    [SerializeField] Image puertaImagen;
+    [SerializeField] Sprite limpio;
+    [SerializeField] Sprite abierto;
     [SerializeField] AudioClip hawktua;
     [SerializeField] AudioClip trapo;
     AudioSource audioSource;
@@ -15,7 +17,7 @@ public class PuertaPantalla : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
     }
-    public void BorrarMancha(Image image)
+    public void BorrarMancha()
     {
         if (!puerta.ManchaBorrada)
         {
@@ -23,30 +25,33 @@ public class PuertaPantalla : MonoBehaviour
             {
                 puerta.ManchaBorrada = true;
                 puerta.QuitarItem(puerta.InteractableDataTrapo.requiredItemID);
-                image.gameObject.SetActive(false);
-                cerradura.gameObject.SetActive(true);
+                puertaImagen.sprite = limpio;
                 audioSource.clip = trapo;
                 audioSource.Play();
             }
         }
     }
-    public void AbrirPuerta(Image image)
+    public void AbrirPuerta()
+    {
+        StartCoroutine(CRAbrirPuerta());
+    }
+    IEnumerator CRAbrirPuerta ()
     {
         if (!puerta.PuertaAbierta && puerta.ManchaBorrada)
         {
             if (puerta.InteractableDataHorquilla.CheckItemRequirement(puerta.InteractableDataHorquilla.requiredItemID))
             {
-                
-                cerradura.gameObject.SetActive(false); 
-                image?.gameObject.SetActive(false); 
-
+                puertaImagen.sprite = abierto;
+                yield return new WaitForSeconds(1);
+                audioSource.clip = hawktua;
+                audioSource.Play();
                 gameObject.SetActive(false);
 
                 puerta.PuertaAbierta = true;
                 puerta.QuitarItem(puerta.InteractableDataHorquilla.requiredItemID);
 
-                audioSource.clip = hawktua;
-                audioSource.Play();
+                
+
             }
         }
     }
